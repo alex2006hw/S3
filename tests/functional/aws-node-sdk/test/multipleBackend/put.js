7e8c505a41454beb3b4f5b8a99030851fc777646
Dora Korpar <dora.korpar@scality.com>
2017-03-03 17:23:53 -0800

    More tests

--- tests/functional/aws-node-sdk/test/multipleBackend/put.js
+++ tests/functional/aws-node-sdk/test/multipleBackend/put.js
@@ -1,17 +1,16 @@
 import assert from 'assert';
 import withV4 from '../support/withV4';
 import BucketUtility from '../../lib/utility/bucket-util';
-import { ds } from '../../../../../lib/data/in_memory/backend';
 
 const bucket = 'buckettestmultiplebackendput';
 const key = 'somekey';
 const body = 'somestring';
+let bucketUtil;
+let s3;
+
 
 describe('MultipleBackend put object', () => {
     withV4(sigCfg => {
-        let bucketUtil;
-        let s3;
-
         beforeEach(() => {
             bucketUtil = new BucketUtility('default', sigCfg);
             s3 = bucketUtil.s3;
@@ -111,25 +110,24 @@ describe('MultipleBackend put object', () => {
     });
 });
 
-describe.only('MultipleBackend put object based on bucket location', () => {
+describe('MultipleBackend put object based on bucket location', () => {
     withV4(sigCfg => {
-        let bucketUtil;
-        let s3;
+        const params = { Bucket: bucket, Key: key, Body: body };
 
-        // afterEach(() => {
-        //     process.stdout.write('Emptying bucket\n');
-        //     return bucketUtil.empty(bucket)
-        //     .then(() => {
-        //         process.stdout.write('Deleting bucket\n');
-        //         return bucketUtil.deleteOne(bucket);
-        //     })
-        //     .catch(err => {
-        //         process.stdout.write(`Error in afterEach: ${err}\n`);
-        //         throw err;
-        //     });
-        // });
+        afterEach(() => {
+            process.stdout.write('Emptying bucket\n');
+            return bucketUtil.empty(bucket)
+            .then(() => {
+                process.stdout.write('Deleting bucket\n');
+                return bucketUtil.deleteOne(bucket);
+            })
+            .catch(err => {
+                process.stdout.write(`Error in afterEach: ${err}\n`);
+                throw err;
+            });
+        });
 
-        it('should put an object to same location as bucket', done => {
+        it('should put an object mem with no location header', done => {
             bucketUtil = new BucketUtility('default', sigCfg);
             s3 = bucketUtil.s3;
             process.stdout.write('Creating bucket\n');
@@ -143,18 +141,38 @@ describe.only('MultipleBackend put object based on bucket location', () => {
                     return err;
                 }
                 process.stdout.write('Putting object\n');
-                const params = { Bucket: bucket, Key: key, Body: body, Metadata: { 'scal-location-constraint': 'mem' } };
                 return s3.putObject(params, err => {
                     assert.equal(err, null, 'Expected success, ' +
                         `got error ${JSON.stringify(err)}`);
-                    console.log('ds in test? ', ds);
-                    s3.getObject( { Bucket: bucket, Key: key }, (err, data) => {
-                        if (err) {
-                            console.log('err: ', err);
-                        }
-                        console.log('got data!!!!!: ', data);
-                        assert.strictEqual(ds[1].keyContext.objectKey, key,
-                            'Object in mem datastore does not match put object');
+                    s3.getObject({ Bucket: bucket, Key: key }, err => {
+                        assert.strictEqual(err, null, 'Expected success, ' +
+                        `got error ${JSON.stringify(err)}`);
+                        done();
+                    });
+                });
+            });
+        });
+
+        it('should put an object to sproxyd with no location header', done => {
+            bucketUtil = new BucketUtility('default', sigCfg);
+            s3 = bucketUtil.s3;
+            process.stdout.write('Creating bucket\n');
+            return s3.createBucket({ Bucket: bucket,
+                CreateBucketConfiguration: {
+                    LocationConstraint: 'scality-us-east-1',
+                },
+            }, err => {
+                if (err) {
+                    process.stdout.write(`Error creating bucket: ${err}\n`);
+                    return err;
+                }
+                process.stdout.write('Putting object\n');
+                return s3.putObject(params, err => {
+                    assert.equal(err, null, 'Expected success, ' +
+                        `got error ${JSON.stringify(err)}`);
+                    s3.getObject({ Bucket: bucket, Key: key }, err => {
+                        assert.strictEqual(err, null, 'Expected succes, ' +
+                            `got error ${JSON.stringify(err)}`);
                         done();
                     });
                 });
@@ -163,6 +181,32 @@ describe.only('MultipleBackend put object based on bucket location', () => {
     });
 });
 
-// sub describe based on bucket location
+// describe('MultipleBackend put based on request endpoint', () => {
+//     withV4(sigCfg => {
+//         before(() => {
+//             bucketUtil = new BucketUtility('default', sigCfg);
+//             s3 = bucketUtil.s3;
+//             process.stdout.write('Creating bucket');
+//             return s3.createBucketAsync({ Bucket: bucket })
+//             .catch(err => {
+//                 process.stdout.write(`Error creating bucket: ${err}\n`);
+//                 throw err;
+//             });
+//         });
 
-// sub describe for buckets with no location -- so based on endpoint
+//         after(() => {
+//             process.stdout.write('Emptying bucket\n');
+//             return bucketUtil.empty(bucket)
+//             .then(() => {
+//                 process.stdout.write('Deleting bucket\n');
+//                 return bucketUtil.deleteOne(bucket);
+//             })
+//             .catch(err => {
+//                 process.stdout.write(`Error in afterEach: ${err}\n`);
+//                 throw err;
+//             });
+//         });
+
+//         it('should ');
+//     });
+// });
